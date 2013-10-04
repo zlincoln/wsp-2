dfd4a8c93ae4742cdca443526b7c9ea0546efa4e
Zach <zach@bluehousegroup.com>
2013-10-04 04:07:28 -0400

    started work on switching between fixed and static header

--- includes/footer.php
+++ includes/footer.php
@@ -1,20 +1,37 @@
 <footer>
 	<div class="container">
 		<div class="row">
-			<div class="copyright-holder">
-				<img src="http://placehold.it/90x57" alt="hg watermark">
-				<span>Copyright &copy;2013 | HG Skis | All rights reserved</span>
+			<div class="col-12">
+				<div class="inner-wrap">
+					<div class="copyright-holder">
+						<img src="http://placehold.it/90x57" alt="hg watermark">
+						<span>Copyright &copy;2013 | HG Skis | All rights reserved</span>
+					</div>
+					<nav class="social-nav pull-right">
+						<ul>
+							<li><a href="http://www.facebook.com/hgskis" target="_blank"><i class="icon icon-facebook"></i></a></li>
+							<li><a href="http://vimeo.com/hgskis" target="_blank"><i class="icon icon-facebook"></i></a></li>
+							<li><a href="http://facebook.com" target="_blank"><i class="icon icon-facebook"></i></a></li>
+							<li><a href="http://facebook.com" target="_blank"><i class="icon icon-facebook"></i></a></li>
+						</ul>
+					</nav>
+				</div>
 			</div>
-			<nav class="social-nav pull-right">
-				<ul>
-					<li><a href="http://www.facebook.com/hgskis" target="_blank"><i class="icon icon-facebook"></i></a></li>
-					<li><a href="http://vimeo.com/hgskis" target="_blank"><i class="icon icon-facebook"></i></a></li>
-					<li><a href="http://facebook.com" target="_blank"><i class="icon icon-facebook"></i></a></li>
-					<li><a href="http://facebook.com" target="_blank"><i class="icon icon-facebook"></i></a></li>
-				</ul>
-			</nav>
 		</div>
 	</div>	
 </footer>
+<script type="text/javascript">
+	//utilities
+	//@todo: remove these at launch
+	function bodyClassHome(){
+		$('body').toggleClass('static-header');
+	}
+	function logoSwitch(){
+		$('.navbar-brand').toggleClass('reduced');
+	}
+	//@todo: set min height for body to window.height
+	//@todo: set flexslider max-height dependant on window.height
+	//@todo: if html.height is less than window.height+header.height apply static-header class to body
+</script>
 </body>
 </html>
