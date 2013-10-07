9812c66b0e69dbee6cc264cf43f384d66dc3b9e8
Zach <zach@bluehousegroup.com>
2013-10-07 09:19:58 -0400

    working on adding interior page markup

---
+++ goods.php
@@ -0,0 +1,49 @@
+<?php include('includes/header.php'); ?>
+<section class="gallery-detail-template">
+	<div class="container">
+		<div id="detail-target" class="row">
+			<div class="image-viewport col-8">
+				<img class="img-responsive" src="http://placehold.it/1200x1000">
+			</div>
+			<div class="item-info">
+				<span class="item-title">The chief coast coach</span>
+				<div class="image-gallery">
+					<img class="img-responsive" src="http://placehold.it/1200x1000">
+					<img class="img-responsive" src="http://placehold.it/1200x1000">
+					<img class="img-responsive" src="http://placehold.it/1200x1000">
+					<img class="img-responsive" src="http://placehold.it/1200x1000">
+				</div>
+				<div class="wysiwyg-content">
+					<p>Limited run: HG Coaches Jacket</p>
+					<span style="font-weight: bold">Size Chart:</span>
+					<ul>
+						<li>M: L:32" W:26.5"</li>
+						<li>L: L:32" W:26.5"</li>
+						<li>XL: L:32" W:26.5"</li>
+						<li>2XL: L:32" W:26.5"</li>
+					</ul>
+					<form>
+						<select>
+							<option>Please Select Size</option>
+							<option>M: L:32" W:26.5"</option>
+							<option>L: L:32" W:26.5"</option>
+							<option>XL: L:32" W:26.5"</option>
+							<option>2XL: L:32" W:26.5"</option>
+						</select>
+						<input type="submit" class="button button-primary" name="submit" id="purchase-item-1" value="BUY">
+					</form>
+				</div>
+			</div>
+		</div>
+		<div id="option-gallery" class="row">
+			<div class="item-wrapper">
+				<div class="img-wrap">
+					<img class="img-responsive" src="http://placehold.it/800x600">
+					<span class="price">$55.00</span>
+				</div>
+				<span class="item-title">The chief coast coach</span>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include('includes/footer.php'); ?>
---
+++ skis.php
@@ -0,0 +1,101 @@
+<?php include('includes/header.php'); ?>
+<section class="ski-detail">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h1><span>Stinger</span><img class="img-responsive" src="http://placehold.it/400x300" alt="Stinger"></h1>
+				<img class="img-responsive" src="http://placehold.it/1200x400">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-8">
+				<h2>Stinger</h2>
+				<div class="wysiwyg-content">
+					<h3>Introducing the full Redesigned 2014 Stinger from HG Skis.</h3>
+					<h4>The only skis on the market designed specifically for east coast terrain.</h4>
+					<p>Offerings in both 178cm and 169cm lengths - 125-96-125 - 18m radius</p>
+					<h5>New Features for 2014</h5>
+					<ul>
+						<li>Increased overall width
+							<ul>
+								<li>Improved stability on landing</li>
+								<li>Better float through crud of off piste</li>
+								<li>Surface swaps IN YOUR SLEEP</li>
+							</ul>
+						</li>
+						<li>Fully mastered flex profile
+							<ul>
+								<li>Increased structure for stability</li>
+								<li>Enough pop for a sugar high</li>
+							</ul>
+						</li>
+						<li>Hand painted graphic
+							<ul>
+								<li>Ride on a work of art</li>
+								<li>Vermont local artist <a href="http://phoebelow.com/" target="_blank" title="Phoebe's Portfolio - Check it out!">Phoebe Low</a></li>
+							</ul>
+						</li>
+						<li>Matte chip-stop topsheet
+							<ul>
+								<li>Scratch resistant</li>
+								<li>No more chipping or flaking</li>
+								<li>Seals core from moisture preventing delamination</li>
+							</ul>
+						</li>
+						<li>Advanced sidewall geometry
+							<ul>
+								<li>Increased edge strength</li>
+								<li>Reduced snow and debris build up</li>
+							</ul>
+						</li>
+						<li>Green ice wax factory finish
+							<ul>
+								<li>Base wax does not contain any harmful fluorocarbons</li>
+								<li>Safe for our ski builders</li>
+								<li>Clean for your mountains</li>
+							</ul>
+						</li>
+					</ul>
+					<h5>The things that already made us the best:</h5>
+					<ul>
+						<li>Zero-delam tip</li>
+						<li>Reinforced super light aspen wood core</li>
+						<li>Super pop</li>
+						<li>Responsive side cut</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-4">
+				<div class="actions">
+					<span class="price">$599</span>
+					<a class="button button-primary">Buy</a>
+				</div>
+				<div class="reviews">
+					<div class="review-wrap">
+						<h6>Best pair of skis I've ever touched</h6>
+						<ul class="rating">
+							<li><i class="icon icon-star"></i></li>
+							<li><i class="icon icon-star"></i></li>
+							<li><i class="icon icon-star"></i></li>
+							<li><i class="icon icon-star"></i></li>
+							<li><i class="icon icon-star"></i></li>
+						</ul>
+						<p>This is a product review submitted by a real customer who loves everything about these skis!  He talks about how much fun they are to ride and how everyone notices the dope graphics and wonders where he got them.</p>
+					</div>
+					<div class="review-wrap">
+						<h6>Best pair of skis I've ever touched</h6>
+						<ul class="rating">
+							<li><i class="icon icon-star"></i></li>
+							<li><i class="icon icon-star"></i></li>
+							<li><i class="icon icon-star"></i></li>
+							<li><i class="icon icon-star"></i></li>
+							<li><i class="icon icon-star"></i></li>
+						</ul>
+						<p>This is a product review submitted by a real customer who loves everything about these skis!  He talks about how much fun they are to ride and how everyone notices the dope graphics and wonders where he got them.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include('includes/footer.php'); ?>
