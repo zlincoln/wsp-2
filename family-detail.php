2da68861b1e1653da32037108af6ff71fba1814a
Zach <zach@bluehousegroup.com>
2013-10-08 22:56:16 -0400

    ready for conversion to html files

---
+++ family-detail.php
@@ -0,0 +1,21 @@
+<?php include('includes/header.php'); ?>
+<section class="gallery-detail-template family container detail-view">
+	<div id="detail-target" class="row" data-curr-detail="null">
+		<div class="item-viewport col-md-8 col-sm-6">
+			<img class="img-responsive" src="images/family/connor.png" alt="Conner Gaeta">
+		</div>
+		<div class="item-details">
+			<h3 class="item-title">Connor Gaeta</h3>
+			<div class="image-gallery">
+				<!-- <img class="img-responsive" src="http://placehold.it/1200x1000">
+				<img class="img-responsive" src="http://placehold.it/1200x1000">
+				<img class="img-responsive" src="http://placehold.it/1200x1000">
+				<img class="img-responsive" src="http://placehold.it/1200x1000"> -->
+			</div>
+			<div class="wysiwyg-content">
+				<p>I got started with HG about 3 years ago well it was still in its infancy stages. From then it's grown out of a dingy garage in downtown Burlington to a real shop. I am always trying to see how I can change my personal style to reflect in HG itself. Working with HG really allows everything we do on the snow reflect in the company itself.</p>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include('includes/footer.php'); ?>
--- family.php
+++ family.php
@@ -3,21 +3,23 @@ class Member{
 	public $name;
 	public $photoURL;
 	public $videoURL;
-	function __constructor($name, $photoURL, $videoURL){
+	public $bio;
+	function __construct($name, $photoURL, $videoURL, $bio){
 		$this->name = $name;
 		$this->photoURL = $photoURL;
 		$this->videoURL = $videoURL;
+		$this->bio = $bio;
 	}
 }
 
-$connor = new Member('Connor Gaeta', 'images/family/conner.jpg', 'http://player.vimeo.com/video/50028011');
-$hunter = new Member('Hunter Tyrell', 'images/family/hunter.jpg', 'http://player.vimeo.com/video/49095760');
-$james = new Member('James Amedeo', 'images/family/james.jpg', 'http://player.vimeo.com/video/46318020');
+$connor = new Member('Connor Gaeta', 'images/family/connor.png', 'http://player.vimeo.com/video/50028011', "<p>I got started with HG about 3 years ago well it was still in its infancy stages. From then it's grown out of a dingy garage in downtown Burlington to a real shop. I am always trying to see how I can change my personal style to reflect in HG itself. Working with HG really allows everything we do on the snow reflect in the company itself.</p>");
+$hunter = new Member('Hunter Tyrell', 'images/family/hunter.png', 'http://player.vimeo.com/video/49095760', "<p>Yo what's good I'm Hunter Tyrrell and I'm a rider for HG skis. I started skiing when I was about 2 and was racing up until I was 11. I always had a love for freestyle skiing ever since I ditched my ski group to take park laps. The first couple of years for me consisted on expanding my bag of tricks and dialing them down consistently.</p>");
+$james = new Member('James Amedeo', 'images/family/james.png', 'http://player.vimeo.com/video/46318020', "<p>Grew up living and skiing on Burke Mountain where I've been for the last 18 years. Moved to Burlington, Vermont over the summer to change things up a bit and ski at Sugarbush this season with the whole HG crew! Shout outs to the boys at HG, YMR, Dan at Arsenic Apparel, Ian at Kingdom Softwear, and all the friends and family that helped me along the way.</p>");
 $riders = array($connor, $hunter, $james);
 
-$harrison = new Member('Harrison Goldberg', 'images/family/harrison.jpg', false);
-$zach = new Member('Zachary Lincoln', 'images/family/zach.jpg', false);
-$charlie = new Member('Charles Stemen', 'images/family/charlie.jpg', false);
+$harrison = new Member('Harrison Goldberg', 'images/family/harrison.png', false, "<p>Growing up if I wasn't building with Legos, I was likely constructing with duct tape and card board, or wiring contraptions that fell a little short of eletrical standards. I skied on weekends and after school, or I snowskated in my back yard if I couldn't get on the hill. In high school I built a pair of skis for the first time as a senior project. Two years later when I blew the circuit for the whole floor in my dorm I decided it was time to focus on just the skis!</p>");
+$zach = new Member('Zachary Lincoln', 'images/family/zach.png', false, "<p>Chicken shit in the park.</p>");
+$charlie = new Member('Charles Stemen', 'images/family/charlie.png', false, "<p>Growing up a weekend warrior, me and some friends started filming each other once in a while at Okemo in my early years of schooling. My last 2 years of high school I started making ski movies with my friend George Watts, and since then I have found skiing coming before almost anything else. Its great to work with a team of equally passionate people here at HG Skis, and as our riders progress, so does our video production. I work on the graphics, videos, and photography - putting in the hours behind the appearance of our products. My video production partner in crime, Sam Rogers, and of course, the team riders, will be hard at work getting you the best quality videos in the 12/13 season that your going to have to watch twice to comprehend. Be sure to say hello if you see us riding/filming!</p>");
 $team = array($harrison, $zach, $charlie);
 
 ?>
@@ -27,10 +29,10 @@ $team = array($harrison, $zach, $charlie);
 	<div class="row item-gallery">
 		<h2 class="col-xs-12">Riders</h2>
 		<?php foreach($riders as $member): ?>
-		<a class="item-wrapper col-xs-12 col-sm-6 col-md-4" href="goods-detail.php">
+		<a class="item-wrapper col-xs-12 col-sm-6 col-md-4" href="family-detail.php">
 			<div class="item-viewport">
-				<img class="img-responsive" src="http://placehold.it/1200x1000">
-				<span class="price">$55.00</span>
+				<img class="img-responsive" src="<?= $member->photoURL; ?>" alt="<?= $member->name; ?>">
+				<iframe src="<?= $member->videoURL; ?>"></iframe>
 			</div>
 			<div class="item-details">
 				<h3 class="item-title"><?= $member->name; ?></h3>
@@ -41,24 +43,7 @@ $team = array($harrison, $zach, $charlie);
 					<img class="img-responsive" src="http://placehold.it/1200x1000"> -->
 				</div>
 				<div class="wysiwyg-content">
-					<p>Limited run: HG Coaches Jacket</p>
-					<span style="font-weight: bold">Size Chart:</span>
-					<ul>
-						<li>M: L:32" W:26.5"</li>
-						<li>L: L:32" W:26.5"</li>
-						<li>XL: L:32" W:26.5"</li>
-						<li>2XL: L:32" W:26.5"</li>
-					</ul>
-					<form>
-						<select>
-							<option>Please Select Size</option>
-							<option>M: L:32" W:26.5"</option>
-							<option>L: L:32" W:26.5"</option>
-							<option>XL: L:32" W:26.5"</option>
-							<option>2XL: L:32" W:26.5"</option>
-						</select>
-						<input type="submit" class="button button-primary" name="submit" id="purchase-item-1" value="BUY">
-					</form>
+					<?= $member->bio; ?>
 				</div>
 			</div>
 		</a>
@@ -66,14 +51,13 @@ $team = array($harrison, $zach, $charlie);
 	</div>
 	<div class="row item-gallery">
 		<h2 class="col-xs-12">Team</h2>
-		<?php for($i=0;$i<=2;$i++): ?>
-		<a class="item-wrapper col-xs-12 col-sm-6 col-md-4" href="goods-detail.php">
+		<?php foreach($team as $member): ?>
+		<a class="item-wrapper col-xs-12 col-sm-6 col-md-4" href="family-detail.php">
 			<div class="item-viewport">
-				<img class="img-responsive" src="http://placehold.it/1200x1000">
-				<span class="price">$55.00</span>
+				<img class="img-responsive" src="<?= $member->photoURL; ?>" alt="<?= $member->name; ?>">
 			</div>
 			<div class="item-details">
-				<h3 class="item-title">The chief coast coach</h3>
+				<h3 class="item-title"><?= $member->name; ?></h3>
 				<div class="image-gallery">
 					<!-- <img class="img-responsive" src="http://placehold.it/1200x1000">
 					<img class="img-responsive" src="http://placehold.it/1200x1000">
@@ -81,28 +65,11 @@ $team = array($harrison, $zach, $charlie);
 					<img class="img-responsive" src="http://placehold.it/1200x1000"> -->
 				</div>
 				<div class="wysiwyg-content">
-					<p>Limited run: HG Coaches Jacket</p>
-					<span style="font-weight: bold">Size Chart:</span>
-					<ul>
-						<li>M: L:32" W:26.5"</li>
-						<li>L: L:32" W:26.5"</li>
-						<li>XL: L:32" W:26.5"</li>
-						<li>2XL: L:32" W:26.5"</li>
-					</ul>
-					<form>
-						<select>
-							<option>Please Select Size</option>
-							<option>M: L:32" W:26.5"</option>
-							<option>L: L:32" W:26.5"</option>
-							<option>XL: L:32" W:26.5"</option>
-							<option>2XL: L:32" W:26.5"</option>
-						</select>
-						<input type="submit" class="button button-primary" name="submit" id="purchase-item-1" value="BUY">
-					</form>
+					<?= $member->bio; ?>
 				</div>
 			</div>
 		</a>
-		<?php endfor; ?>
+		<?php endforeach; ?>
 	</div>
 </section>
 <?php include('includes/gallery-script.php'); ?>
